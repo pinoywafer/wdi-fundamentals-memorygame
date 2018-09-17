d6003ddf4f1d6f757900152a15430580d30c045e
Jonathan Villanueva <jonathan.villanueva@altium.biz>
2018-09-16 20:59:49 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+}, 
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+}, 
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+}, 
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
 var checkForMatch = function (){
@@ -10,17 +31,19 @@ var checkForMatch = function (){
 }
 
 var flipCard = function(cardId){
-	console.log("User Flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User Flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+
+	cardsInPlay.push(cards[cardId].rank);
 
 	if (cardsInPlay.length === 2){
 		checkForMatch();
 	}
-
-	
 }
 
 flipCard(0);
-flipCard(1);
+flipCard(2);
+
 
 
